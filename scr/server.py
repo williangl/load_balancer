8675957fb29c4b6b84d689acecb85a767b4f8d4e
willian GL <williangldzn@gmail.com>
2019-07-17 20:51:10 -0300

    setando método is_full como atributo na classe Server

--- scr/server.py
+++ scr/server.py
@@ -4,11 +4,11 @@
 class Server:
     def __init__(self):
         self.task_pool = []
-        # self.full = False
 
     def add_task(self):
         self.task_pool.append(None)
 
+    @property
     def is_full(self):
         if len(self.task_pool) >= 2:
             return True
